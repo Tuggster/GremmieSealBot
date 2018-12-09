2f429c8798c31aec817567f09202d151e01d640f
Tuggster <paxtonphillips@outlook.com>
2018-12-09 13:41:26 -0800

    Added webportal support!
    Pretty cool, hopefully.

---
+++ GremmiePacks/gsbweb.js
@@ -0,0 +1,38 @@
+const express = require(`express`);
+var app = express();
+
+app.get('/data', function(req, res){
+  res.send('Testing, 123!'); 
+});
+
+app.listen(3000);
+
+module.exports = function() {
+
+  var module = {};
+
+  var data = {
+    client: undefined,
+    discord: undefined,
+    sql: undefined
+  }
+
+
+
+  module.data = data;
+
+  module.loadData = function(sql, client, discord) {
+    data.sql = sql;
+    data.client = client;
+    data.discord = discord;
+  }
+
+  module.name = "GremmieWeb";
+  module.desc = "The bridge between GSB and the GSB web portal!";
+
+  module.onBotReady = function() {
+    console.log("GSB web service ready!");
+  }
+
+  return module;
+}
