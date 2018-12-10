5680cae652a3932b833d9d89d701950858f50901
Tuggster <paxtonphillips@outlook.com>
2018-12-09 16:38:01 -0800

    Update gsbweb.js

--- GremmiePacks/gsbweb.js
+++ GremmiePacks/gsbweb.js
@@ -2,6 +2,12 @@ const express = require(`express`);
 var cors = require('cors')
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/data', function(req, res, next) {
   res.__setitem__("Access-Control-Allow-Origin", "*")
 
