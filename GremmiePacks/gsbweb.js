146b1a53fd0a51e7148b2bbf9686b4740a850069
Tuggster <paxtonphillips@outlook.com>
2018-12-10 19:36:09 -0800

    Testing getting my stats from the website
    Just a quick test!

--- GremmiePacks/gsbweb.js
+++ GremmiePacks/gsbweb.js
@@ -12,7 +12,11 @@ app.use(cors());
 
 app.get('/data/', function(req, res, next) {
   res.withCredentials = true;
-  res.send(`GSB's last message: ${data.client.user.lastMessage.content}`);
+  res.setHeader('Content-Type', 'application/json');
+
+  sql.get(`SELECT * FROM scores WHERE userId ="291754013131538432"`).then(row => { // Grab the sender's GremmieStats profile.
+    res.send(JSON.stringify({ name:  row.userId, sent: row.gremmiesGiven, received: row.gremmiesRecieved}));
+  });
 });
 
 app.listen(3000);
