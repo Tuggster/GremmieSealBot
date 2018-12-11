4a5ab33e899614dfaa064c2ad7051b1b5c3eaf59
Tuggster <paxtonphillips@outlook.com>
2018-12-10 19:37:12 -0800

    Update gsbweb.js

--- GremmiePacks/gsbweb.js
+++ GremmiePacks/gsbweb.js
@@ -14,7 +14,7 @@ app.get('/data/', function(req, res, next) {
   res.withCredentials = true;
   res.setHeader('Content-Type', 'application/json');
 
-  sql.get(`SELECT * FROM scores WHERE userId ="291754013131538432"`).then(row => { // Grab the sender's GremmieStats profile.
+  data.sql.get(`SELECT * FROM scores WHERE userId ="291754013131538432"`).then(row => { // Grab the sender's GremmieStats profile.
     res.send(JSON.stringify({ name:  row.userId, sent: row.gremmiesGiven, received: row.gremmiesRecieved}));
   });
 });
